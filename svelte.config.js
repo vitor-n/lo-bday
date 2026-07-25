5f1b124fad854df39ab653c5eb8dbb6f4d010350
vitor do nascimento <45907317+vitor-n@users.noreply.github.com>
2026-07-25 12:41:34 -0300

    Configure gh-pages deployment with SvelteKit adapter-static

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	compilerOptions: {
+		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+	},
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.BASE_PATH !== undefined ? process.env.BASE_PATH : (process.argv.includes('dev') ? '' : '/lo-bday')
+		}
+	}
+};
+
+export default config;
